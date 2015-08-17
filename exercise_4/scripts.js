73faaf1519e217c903ee09fd28b26b5c389bb7c0
Tyler <tyler@future-ink.com>
2015-08-17 16:46:34 -0700

    sum and range function

--- exercise_4/scripts.js
+++ exercise_4/scripts.js
@@ -1,28 +1,46 @@
 (function() {
 
-  function range (start, end) {
 
-    var storedArray = [];
+  // function sum (n) {
+  //   return n[2];
+  //   //return n.length;
+  //   // var total = n[n.length];
+  //   // for (var i = n.length; i > 0; i--) {
+  //   //   total += n[i-1];
+  //   // }
+  //   // return total;
+  // }
 
-    for (var n = 0; n < 10; n++) {
-      storedArray[n] = n + 1;
-    }
-
-    return storedArray;
-
-  }
-
-  function sum (array) {
-    var total = 0;
-    for (var i = 0; i < array.length; i++) {
-      total = parseInt(array[i]) + parseInt(array[i+1]); 
-    }
-
-    return total;
-
-  }
-
-  var test = sum(range(1,10));
-  console.log(parseInt(test));
+  //console.log(range(1,10));
+  console.log(sum(range(1,10)));
 
 })(); 
+
+// range(1,10);
+function range (start, end) {
+
+  var storedRange = [];
+
+  for (var n = 0; n <= (end - start); n++) {
+    if (n == 0) {
+      storedRange[n] = start;
+    } else {
+      storedRange[n] = storedRange[n - 1] + 1;
+    }
+  }
+  return storedRange;
+}
+
+function sum(range) {
+
+  var total = 0;
+
+  for(var n = 0; n < range.length; n++) {
+
+    total = range[n] + range[n+1];
+
+  }
+
+  return total;
+}
+
